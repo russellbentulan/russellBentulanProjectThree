24ad28d9f6e3eb44a08f65de261bcc7b69b4b04c
Russell Bentulan <russellbentulan@gmail.com>
2019-11-14 20:30:35 -0500

    added data 🍤

---
+++ scripts/script.js
@@ -0,0 +1,101 @@
+// NAMESPACE OBJECT
+app = {};
+
+// INGREDIENT INFORMATION
+app.ingredients = [
+    {
+        name: 'strawberries',
+        type: 'fruit',
+        nutrients: ['folate', 'potassium', 'fiber', 'vitamin c']
+    },
+    {
+        name: 'blueberries',
+        type: 'fruit',
+        nutrients: ['vitamin e', 'folate', 'magnessium', 'postassium', 'fiber', 'vitamin c', 'vitamin k', 'manganese']
+    },
+    {
+        name: 'blueberries',
+        type: 'fruit',
+        nutrients: ['vitamin e', 'folate', 'magnesium', 'potassium', 'fiber', 'vitamin c', 'vitamin k', 'manganese']
+    },
+    {
+        name: 'blackberries',
+        type: 'fruit',
+        nutrients: ['vitamin e', 'folate', 'magnesium', 'potassium', 'fiber', 'vitamin c', 'vitamin k', 'manganese']
+    },
+    {
+        name: 'bananas',
+        type: 'fruit',
+        nutrients: ['fiber', 'vitamin c', 'potassium', 'manganese', 'vitamin b6']
+    },
+    {
+        name: 'pineapple',
+        type: 'fruit',
+        nutrients: ['thiamin', 'vitamin b6', 'copper', 'vitamin c', 'manganese']
+    },
+    {
+        name: 'mango',
+        type: 'fruit',
+        nutrients: ['fiber', 'vitamin b6', 'vitamin a', 'vitamin c']
+    },
+    {
+        name: 'peach',
+        type: 'fruit',
+        nutrients: ['fiber', 'vitamin a', 'niacin', 'potassium', 'vitamin c']
+    },
+    {
+        name: 'plain yogurt',
+        type: 'thickener',
+        nutrients: ['protein', 'riboflavin', 'calcium', 'phosphorus']
+    },
+    {
+        name: 'peanut butter',
+        type: 'thickener',
+        nutrients: ['niacin', 'manganese']
+    },
+    {
+        name: 'almond butter',
+        type: 'thickener',
+        nutrients: ['magnesium', 'manganese']
+    },
+    {
+        name: 'almond milk',
+        type: 'liquid',
+        nutrients: ['vitamin e', 'riboflavin', 'magnesium', 'calcium', 'potassium']
+    },
+    {
+        name: 'coconut milk',
+        type: 'liquid',
+        nutrients: ['menganese']
+    },
+    {
+        name: 'whole milk',
+        type: 'liquid',
+        nutrients: ['vitamin d', 'calcium', 'phosphorus']
+    },
+    {
+        name: 'spinach',
+        type: 'mixin',
+        nutrients: ['niacin', 'zinc', 'fiber', 'protein', 'vitamin a', 'vitamin c', 'vitamin e', 'vitamin k', 'thiamin', 'robiflavin', 'vitamin b6', 'folate', 'calcium', 'iron', 'magnesium', 'potassium']
+    },
+    {
+        name: 'kale',
+        type: 'mixin',
+        nutrients: ['fiber', 'protein', 'thiamin', 'riboflavin', 'folate', 'iron', 'magnesium', 'vitamin a', 'vitamin c', 'vitamin k', 'vitamin b6', 'calcium', 'potassium', 'copper', 'manganese']
+    },
+    {
+        name: 'flax seed',
+        type: 'mixin',
+        nutrients: ['fiber', 'protein', 'thiamine', 'copper', 'magnesium', 'phosphorous']
+    }
+];
+
+// INIT FUNCTION
+app.init = () => {
+
+};
+
+// DOCUMENT READY
+$(function() {
+    app.init();
+});
