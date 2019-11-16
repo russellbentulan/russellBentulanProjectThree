143a8c114d6356368e92a8847740d7bb515c9e87
Russell Bentulan <russellbentulan@gmail.com>
2019-11-16 18:27:07 -0500

    added ingredient images, changed layout 🥥🥭🍑

--- scripts/script.js
+++ scripts/script.js
@@ -6,87 +6,103 @@ app.ingredients = [
     {
         name: 'strawberries',
         type: 'fruit',
-        nutrients: ['folate', 'potassium', 'fiber', 'vitamin c']
+        nutrients: ['folate', 'potassium', 'fiber', 'vitamin c'],
+        img: 'strawberries.jpg'
     },
     {
         name: 'blueberries',
         type: 'fruit',
-        nutrients: ['vitamin e', 'folate', 'magnessium', 'postassium', 'fiber', 'vitamin c', 'vitamin k', 'manganese']
+        nutrients: ['vitamin e', 'folate', 'magnessium', 'postassium', 'fiber', 'vitamin c', 'vitamin k', 'manganese'],
+        img: 'blueberries.jpg'
     },
     {
         name: 'blackberries',
         type: 'fruit',
-        nutrients: ['vitamin e', 'folate', 'magnesium', 'potassium', 'fiber', 'vitamin c', 'vitamin k', 'manganese']
+        nutrients: ['vitamin e', 'folate', 'magnesium', 'potassium', 'fiber', 'vitamin c', 'vitamin k', 'manganese'],
+        img: 'blackberries.jpg'
     },
     {
-        name: 'bananas',
+        name: 'banana',
         type: 'fruit',
-        nutrients: ['fiber', 'vitamin c', 'potassium', 'manganese', 'vitamin b6']
+        nutrients: ['fiber', 'vitamin c', 'potassium', 'manganese', 'vitamin b6'],
+        img: 'banana.jpg'
     },
     {
         name: 'pineapple',
         type: 'fruit',
-        nutrients: ['thiamin', 'vitamin b6', 'copper', 'vitamin c', 'manganese']
+        nutrients: ['thiamin', 'vitamin b6', 'copper', 'vitamin c', 'manganese'],
+        img: 'pineapple.jpg'
     },
     {
         name: 'mango',
         type: 'fruit',
-        nutrients: ['fiber', 'vitamin b6', 'vitamin a', 'vitamin c']
+        nutrients: ['fiber', 'vitamin b6', 'vitamin a', 'vitamin c'],
+        img: 'mango.jpg'
     },
     {
         name: 'peach',
         type: 'fruit',
-        nutrients: ['fiber', 'vitamin a', 'niacin', 'potassium', 'vitamin c']
+        nutrients: ['fiber', 'vitamin a', 'niacin', 'potassium', 'vitamin c'],
+        img: 'peach.jpg'
     },
     {
         name: 'plain yogurt',
         type: 'thickener',
-        nutrients: ['protein', 'riboflavin', 'calcium', 'phosphorus']
+        nutrients: ['protein', 'riboflavin', 'calcium', 'phosphorus'],
+        img: 'plainYogurt.jpg'
     },
     {
         name: 'peanut butter',
         type: 'thickener',
-        nutrients: ['niacin', 'manganese']
+        nutrients: ['niacin', 'manganese'],
+        img: 'peanutButter.jpg'
     },
     {
         name: 'almond butter',
         type: 'thickener',
-        nutrients: ['magnesium', 'manganese']
+        nutrients: ['magnesium', 'manganese'],
+        img: 'almondButter.jpg'
     },
     {
         name: 'almond milk',
         type: 'liquid',
-        nutrients: ['vitamin e', 'riboflavin', 'magnesium', 'calcium', 'potassium']
+        nutrients: ['vitamin e', 'riboflavin', 'magnesium', 'calcium', 'potassium'],
+        img: 'almondMilk.jpg'
     },
     {
         name: 'coconut milk',
         type: 'liquid',
-        nutrients: ['menganese']
+        nutrients: ['menganese'],
+        img: 'coconutMilk.jpg'
     },
     {
         name: 'whole milk',
         type: 'liquid',
-        nutrients: ['vitamin d', 'calcium', 'phosphorus']
+        nutrients: ['vitamin d', 'calcium', 'phosphorus'],
+        img: 'wholeMilk.jpg'
     },
     {
         name: 'spinach',
         type: 'mixin',
-        nutrients: ['niacin', 'zinc', 'fiber', 'protein', 'vitamin a', 'vitamin c', 'vitamin e', 'vitamin k', 'thiamin', 'robiflavin', 'vitamin b6', 'folate', 'calcium', 'iron', 'magnesium', 'potassium']
+        nutrients: ['niacin', 'zinc', 'fiber', 'protein', 'vitamin a', 'vitamin c', 'vitamin e', 'vitamin k', 'thiamin', 'robiflavin', 'vitamin b6', 'folate', 'calcium', 'iron', 'magnesium', 'potassium'],
+        img: 'spinach.jpg'
     },
     {
         name: 'kale',
         type: 'mixin',
-        nutrients: ['fiber', 'protein', 'thiamin', 'riboflavin', 'folate', 'iron', 'magnesium', 'vitamin a', 'vitamin c', 'vitamin k', 'vitamin b6', 'calcium', 'potassium', 'copper', 'manganese']
+        nutrients: ['fiber', 'protein', 'thiamin', 'riboflavin', 'folate', 'iron', 'magnesium', 'vitamin a', 'vitamin c', 'vitamin k', 'vitamin b6', 'calcium', 'potassium', 'copper', 'manganese'],
+        img: 'kale.jpg'
     },
     {
         name: 'flax seed',
         type: 'mixin',
-        nutrients: ['fiber', 'protein', 'thiamine', 'copper', 'magnesium', 'phosphorous']
+        nutrients: ['fiber', 'protein', 'thiamine', 'copper', 'magnesium', 'phosphorous'],
+        img: 'flaxSeed.jpg'
     }
 ];
 
 // GLOBAL VARIABLES
-app.selectedIngredients = app.ingredients.filter(ingredient => ingredient.name === 'strawberries' || ingredient.name === 'almond milk');
+app.selectedIngredients = [];
 
 // Display the selected ingredients onto the page
 // If there are no selected ingredients, display a message instead
@@ -136,23 +152,35 @@ app.displaySelectedIngredients = () => {
 
 // Display all of the ingredient information inside a new element
 app.displayIngredientInfo = function() {
+    // Clear the element each time a new ingredient is chosen
+    app.$infoContainer.empty();
+
     // Get the ingredient name that was just clicked
     const ingredientName = $(this).text().trim();
 
     // Get the information for the ingredient
     const ingredientInfo = app.ingredients.find(ingredient => ingredient.name === ingredientName);
 
+    // Display a list of ingredient nutritional information
     const nutrientsHtml = ingredientInfo.nutrients.map(nutrient => `<li class="ingredientInfoItem">${nutrient}</li>`);
+
+    // Display all of the ingredient information
     const htmlToAppend = `
         <article class="ingredientInfo">
-            <h3 class="ingredientInfoTitle">${ingredientInfo.name}</h3>
-            <ul class="ingredientInfoList">
-                ${nutrientsHtml.join('\n')}
-            </ul>
+            <div class="ingredientInfoImg">
+                <img src="./assets/ingredientPictures/${ingredientInfo.img}" alt="${ingredientInfo.name}, a ${ingredientInfo.type} ingredient for smoothies">
+            </div>
+            <div class="ingredientInfoText">
+                <h3 class="ingredientInfoTitle">${ingredientInfo.name}</h3>
+                
+                <ul class="ingredientInfoList">
+                    ${nutrientsHtml.join('\n')}
+                </ul>
+            </div>
         </article>
     `;
 
-    app.$availabeContainer.append(htmlToAppend);
+    app.$infoContainer.append(htmlToAppend);
 }
 
 // Display all available ingredients
@@ -173,7 +201,6 @@ app.displayAvailableIngredients = () => {
         app.$availabeContainer.append(htmlToAppend);
     });
 
-    const $ingredientsList = $('.availableIngredients');
     const $parentElement = $('.availableIngredients').closest('section');
 
     // Show all ingredients, disable the ingredients selected by the user
@@ -181,7 +208,7 @@ app.displayAvailableIngredients = () => {
 
         // check if the object exists in the selectedIngredients array
         // change the status of the button if the ingredient is already selected
-        let status = 'active';
+        let status = '';
         const selected = app.selectedIngredients.filter(selectedIngredient => selectedIngredient.name === ingredient.name);
         if (selected.length) {
             status = 'disabled';
@@ -190,7 +217,7 @@ app.displayAvailableIngredients = () => {
         // append the item to the appropriate category list
         const htmlToAppend = `
             <li class="availableIngredientsItem">
-                <button class="availableIngredientsButton ${status}">${ingredient.name}</button>
+                <button class="availableIngredientsButton" ${status}>${ingredient.name}</button>
             </li>
         `;
         $parentElement.find(`#${ingredient.type}Category`).append(htmlToAppend);
@@ -206,6 +233,7 @@ app.init = () => {
     app.$addButton = $('.addIngredientButton');
     app.$selectedList = $('.selectedIngredients');
     app.$availabeContainer = $('.availableIngredientsContainer');
+    app.$infoContainer = $('.ingredientInfoContainer');
 
     app.displaySelectedIngredients();
 
